dd9e6438dbe2fd4ed01566dc336aae3872d2e1b7
JangHY <hyunyeong93@gmail.com>
2018-11-25 00:18:24 +0900

    connect MongoDB, create new ID http://localhost:3000/public/adduser.html

--- practice1.js
+++ practice1.js
@@ -6,6 +6,140 @@ var expressSession = require('express-session');//세션
 var multer = require('multer');//파일업로드용
 var fs = require('fs');//파일업로드용
 var cors = require('cors');//다중 서버 접속 지원
+var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+
+var database;
+var UserSchema;
+var UserModel;
+
+function connectDB(){
+    var databaseUrl = "mongodb://localhost:27017/local";
+
+    console.log('데이터 베이스 연결을 시도합니다.');
+    mongoose.Promise = global.Promise;
+    mongoose.connect(databaseUrl);
+    database = mongoose.connection;
+
+    database.on('error',console.error.bind(console, 'mongoose connection erre'));
+    database.on('open',function(){
+        console.log('데이터베이스에 연결되었습니다.:'+databaseUrl);
+
+        UserSchema = mongoose.Schema({
+            id: {type:String, require:true, unique:true},
+            password: {type:String, require:true},
+            name:{type:String, index:'hashed'},
+            age: {type:Number, 'default':-1},
+            created_at:{type:Date, index:{unique:false},'default':Date.now},
+            updated_at:{type:Date, index:{unique:false},'default':Date.now}
+        });
+
+        // UserSchema.static('findById', function(id, callback){
+        //     return this.find({id:id},callback);
+        // });
+
+        // UserSchema.static('findAll',function(callback){
+        //     return this.find({},callback);
+        // })
+
+
+        console.log('UserSchema 정의함.');
+
+        UserModel = mongoose.model("users",UserSchema);
+        console.log('UserModel 정의함');
+    });
+
+    database.on('disconnected',function(){
+        console.log("연결이 끊어졌습니다. 5초 후 다시 연결합니다.");
+        setInterval(connectDB, 5000);
+    })
+
+
+    // MongoClient.connect(databaseUrl, function(err, db){
+    //     if(err) throw err;
+
+    //     console.log('데이터 베이스에 연결되었습니다. : '+databaseUrl);
+
+    //     database = db.db('local');//mongoDB 3.0 이상부터 이름 명시 필요
+    // });
+}
+
+var authUser = function(database, id, password, callback){
+    console.log('authUser 호출됨 : '+id+", "+password);
+
+    UserModel.find({"id":id,"password":password},function(err,results){
+        if(err){
+            callback(err,null);
+            return;
+        }
+
+        console.log('아이디 [%s], 비밀번호 [%s]로 사용자 검색 결과',id, password);
+        console.log(results);
+
+        if(results.length>0){
+            console.log('아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음',id, password);
+            callback(null,results);
+        }
+        else{
+            console.log("일치하는 사용자를 찾지 못함.");
+            callback(null,null);
+        }
+    })
+
+
+    // var users = database.collection('users');
+
+    // users.find({"id":id, "password":password}).toArray(function(err,docs){
+
+    //     if(err){
+    //         callback(err,null);
+    //         return;
+    //     }
+
+    //     if(docs.length>0){
+    //         console.log('아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음',id, password);
+    //         callback(null,docs);
+    //     }
+    //     else{
+    //         console.log("일치하는 사용자를 찾지 못함.");
+    //         callback(null,null);
+    //     }
+    // });
+}
+
+var addUser = function(database, id, password, name, callback){
+    console.log('addUser 호출됨 : '+id+", "+password+", "+name);
+
+    var user = new UserModel({"id":id,"password":password,"name":name});
+
+    user.save(function(err){
+        if(err){
+            callback(err,null);
+            return;
+        }
+        console.log("사용자 데이터 추가함");
+        callback(null,user);
+    });
+
+
+    // var users = database.collection('users');
+
+    // users.insertMany([{"id":id,"password":password,"name":name}],function(err,result){
+    //     if(err){
+    //         callback(err,null);
+    //         return;
+    //     }
+
+    //     if(result.insertedCount > 0){
+    //         console.log("사용자 레코드 추가됨 : "+result.insertedCount);
+    //     }
+    //     else{
+    //         console.log("추가된 레코드가 없음");
+    //     }
+
+    //     callback(null,result);
+    // });
+}
 
 
 var app = express();
@@ -52,29 +186,62 @@ var upload = multer({
 var router = express.Router();
 
 router.route('/process/login').post(function(req,res){
-    console.log('/process/login 처리함');
+    console.log('/process/login 호출됨');
 
     var paramId = req.body.id||req.query.id;
     var paramPassword = req.body.password||req.query.password;
 
-    if(req.session.user){
-        console.log('이미 로그인 되어 상품 페이지로 이동합니다.');
-        res.redirect('/public/product.html');
-    }else{
-        req.session.user = {
-            id: paramId,
-            name: '소녀시대',
-            authorized: true
-        }
-    }
+    // if(req.session.user){
+    //     console.log('이미 로그인 되어 상품 페이지로 이동합니다.');
+    //     res.redirect('/public/product.html');
+    // }else{
+    //     req.session.user = {
+    //         id: paramId,
+    //         name: '소녀시대',
+    //         authorized: true
+    //     }
+    // }
+
+    if (database) {
+		authUser(database, paramId, paramPassword, function(err, docs) {
+			if (err) {throw err;}
+			
+            // 조회된 레코드가 있으면 성공 응답 전송
+			if (docs) {
+				console.dir(docs);
+
+                // 조회 결과에서 사용자 이름 확인
+				var username = docs[0].name;
+				
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h1>로그인 성공</h1>');
+				res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
+				res.write('<div><p>사용자 이름 : ' + username + '</p></div>');
+				res.write("<br><br><a href='/public/login2.html'>다시 로그인하기</a>");
+				res.end();
+			
+			} else {  // 조회된 레코드가 없는 경우 실패 응답 전송
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h1>로그인  실패</h1>');
+				res.write('<div><p>아이디와 패스워드를 다시 확인하십시오.</p></div>');
+				res.write("<br><br><a href='/public/login2.html'>다시 로그인하기</a>");
+				res.end();
+			}
+		});
+	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.write('<div><p>데이터베이스에 연결하지 못했습니다.</p></div>');
+		res.end();
+	}
 
 
-    res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
-    res.write('<h1>express에서 응답한 결과</h1>');
-    res.write('<h1>express에서 응답 '+paramId+' </h1>');
-    res.write('<h1>express에서 응답 '+paramPassword+' </h1>');
-    res.write("<br><br><a href='/process/product'>상품 페이지로 돌아가기</a>");
-    res.end();
+    // res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
+    // res.write('<h1>express에서 응답한 결과</h1>');
+    // res.write('<h1>express에서 응답 '+paramId+' </h1>');
+    // res.write('<h1>express에서 응답 '+paramPassword+' </h1>');
+    // res.write("<br><br><a href='/process/product'>상품 페이지로 돌아가기</a>");
+    // res.end();
     
 });
 
@@ -92,18 +259,57 @@ router.route('/process/logout').get(function(req,res){
         console.log("아직 로그인 안된 상태");
         res.redirect('/public/login2.html');
     }
-})
-
-router.route('/process/users/:id').get(function(req,res){
-    console.log('/porcess/users/:id 처리함')
-
-    var paramId = req.params.id;
-
-    res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
-    res.write('<h1>express에서 응답한 결과</h1>');
-    res.write('<h1>paramId : '+paramId+' </h1>');
-    res.end();
 });
+
+router.route('/process/adduser').post(function(req, res) {
+	console.log('/process/adduser 호출됨.');
+
+    var paramId = req.body.id || req.query.id;
+    var paramPassword = req.body.password || req.query.password;
+    var paramName = req.body.name || req.query.name;
+	
+    console.log('요청 파라미터 : ' + paramId + ', ' + paramPassword + ', ' + paramName);
+    
+    // 데이터베이스 객체가 초기화된 경우, addUser 함수 호출하여 사용자 추가
+	if (database) {
+		addUser(database, paramId, paramPassword, paramName, function(err, result) {
+			if (err) {throw err;}
+			
+            // 결과 객체 확인하여 추가된 데이터 있으면 성공 응답 전송
+            console.log("debug");
+            console.log(result);
+            console.log(result.insertedCount);
+            if (result) {
+				console.dir(result);
+ 
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 추가 성공</h2>');
+				res.end();
+			} else {  // 결과 객체가 없으면 실패 응답 전송
+				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				res.write('<h2>사용자 추가  실패</h2>');
+				res.end();
+			}
+		});
+	} else {  // 데이터베이스 객체가 초기화되지 않은 경우 실패 응답 전송
+		res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+		res.write('<h2>데이터베이스 연결 실패</h2>');
+		res.end();
+	}
+	
+});
+
+
+// router.route('/process/users/:id').get(function(req,res){
+//     console.log('/porcess/users/:id 처리함')
+
+//     var paramId = req.params.id;
+
+//     res.writeHead('200',{'Content-Type':'text/html;charset=utf8'});
+//     res.write('<h1>express에서 응답한 결과</h1>');
+//     res.write('<h1>paramId : '+paramId+' </h1>');
+//     res.end();
+// });
 
 router.route('/process/showCookie').get(function(req,res){
     console.log('process/showCookie 호출됨');
@@ -149,7 +355,7 @@ router.route('/process/photo').post(upload.array('photo',1), function(req,res){
         size = 0;
     
     if (Array.isArray(files)) {   // 배열에 들어가 있는 경우 (설정에서 1개의 파일도 배열에 넣게 했음)
-        console.log("배열에 들어있는 파일 갯수 : %d", files.length);
+        console.log("배열에 들어있는 파일 개수 : %d", files.length);
         
         for (var index = 0; index < files.length; index++) {
             originalname = files[index].originalname;
@@ -159,7 +365,7 @@ router.route('/process/photo').post(upload.array('photo',1), function(req,res){
         }
         
     } else {   // 배열에 들어가 있지 않은 경우 (현재 설정에서는 해당 없음)
-        console.log("파일 갯수 : 1 ");
+        console.log("파일 개수 : 1 ");
         
         originalname = files[index].originalname;
         filename = files[index].name;
@@ -206,6 +412,8 @@ app.use(errorHandler);
 
 http.createServer(app).listen(app.get('port'),function(){
     console.log('익스프레스 서버 시작 : '+app.get('port'));
+
+    connectDB();
 });
 
 
